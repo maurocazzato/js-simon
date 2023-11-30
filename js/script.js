83d33351d075b912e293ba8c1ef5ce9c63a5c1ce
Maurocazzato <maurizzio.125@gmail.com>
2023-11-30 13:57:56 +0100

    preparazione

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Descrizione:
+// Visualizzare in pagina 5 numeri casuali.
+// Da lì parte un timer di 30 secondi. Dopo 30 secondi i numeri scompaiono e l’utente deve inserire, i numeri che ha visto precedentemente.
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
